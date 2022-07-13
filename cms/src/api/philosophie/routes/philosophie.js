a6a4669d4558ef09bddef7458cd82353b8f16aab
Gaelle Londoz <gaelle@MacBook-Pro-de-Gaelle.local>
2022-07-14 00:47:12 +0200

    testing contact netlify

---
+++ cms/src/api/philosophie/routes/philosophie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * philosophie router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::philosophie.philosophie');
---
+++ cms/src/api/philosophie/services/philosophie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * philosophie service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::philosophie.philosophie');
