797e413a6a85d3facfcd24f991e4d282fa0fb8bc
Gaelle Londoz <gaelle@MacBook-Pro-de-Gaelle.local>
2022-07-14 21:33:30 +0200

    testing form

--- frontend/src/routes/contact/index.svelte
+++ frontend/src/routes/contact/index.svelte
@@ -2,12 +2,29 @@
   export const prerender = true;
 </script>
 
+<script>
+  let form = null;
+
+  const handleSubmit = () => {
+    let formData = new FormData(form);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+</script>
+
 <form
   name="contact"
   method="post"
   netlify
   netlify-honeypot="bot-field"
   data-netlify="true"
+  on:submit|preventDefault={handleSubmit}
+  bind:this={form}
 >
   <input type="hidden" name="form-name" value="contact" />
   <input type="text" name="bot-field" />
