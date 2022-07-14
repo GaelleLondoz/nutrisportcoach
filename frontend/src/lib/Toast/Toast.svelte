6c01a685adb81d228b4bf4d098fd25bbffdd6eda
Gaelle Londoz <gaelle@MacBook-Pro-de-Gaelle.local>
2022-07-15 01:49:21 +0200

    form style

---
+++ frontend/src/lib/Toast/Toast.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { fly } from "svelte/transition";
+  import { Icon, X } from "svelte-hero-icons";
+
+  export const open = () => {
+    active = true;
+    timer = setTimeout(() => {
+      active = false;
+    }, 7500);
+  };
+
+  const close = () => {
+    active = false;
+    clearTimeout(timer);
+  };
+
+  let active = false;
+  let timer = null;
+</script>
+
+{#if active}
+  <div class="toast" transition:fly={{ x: -200, duration: 400 }}>
+    <div class="toast-content">
+      <slot />
+    </div>
+    <span on:click={close}>
+      <Icon src={X} size="16" class="close" />
+    </span>
+  </div>
+{/if}
+
+<style lang="scss">
+  @import "./Toast.scss";
+</style>
--- frontend/src/routes/contact/index.svelte
+++ frontend/src/routes/contact/index.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <script>
+  import Toast from "$lib/Toast/Toast.svelte";
+  import { Icon, CheckCircle, XCircle, X } from "svelte-hero-icons";
+
   let form = null;
+  let toast = null;
+  let formSentSuccess = false;
 
   const handleSubmit = () => {
     let formData = new FormData(form);
@@ -12,38 +17,98 @@
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(() => console.log("Form successfully submitted"))
-      .catch((error) => alert(error));
+      .then(() => {
+        toast.open();
+        formSentSuccess = true;
+      })
+      .catch(() => {
+        toast.open();
+        formSentSuccess = false;
+      });
   };
 </script>
 
-<form
-  name="contact"
-  method="post"
-  netlify
-  netlify-honeypot="bot-field"
-  data-netlify="true"
-  on:submit|preventDefault={handleSubmit}
-  bind:this={form}
->
-  <input type="hidden" name="form-name" value="contact" />
-  <input type="text" name="bot-field" />
-  <p>
-    <label>Nom: <input type="text" name="first-name" /></label>
-  </p>
-  <p>
-    <label>Prénom: <input type="text" name="last-name" /></label>
-  </p>
-  <p>
-    <label>Téléphone: <input type="text" name="phone" /></label>
-  </p>
-  <p>
-    <label>Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+<section class="container">
+  <div />
+  <div class="form__container">
+    <h1>Formulaire de contact</h1>
+
+    <form
+      name="contact"
+      method="post"
+      netlify
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+      on:submit|preventDefault={handleSubmit}
+      bind:this={form}
+    >
+      <div class="form__inputs">
+        <input type="hidden" name="form-name" value="contact" />
+        <!-- <input type="text" name="bot-field" /> -->
+
+        <label
+          >Nom <sup>*</sup><input
+            type="text"
+            name="first-name"
+            placeholder="Entrez votre nom"
+            required
+          /></label
+        >
+
+        <label
+          >Prénom <sup>*</sup><input
+            type="text"
+            name="last-name"
+            placeholder="Entrez votre prénom"
+            required
+          /></label
+        >
+
+        <label
+          >E-mail <sup>*</sup><input
+            type="email"
+            name="email"
+            placeholder="Entrez votre e-mail"
+            required
+          /></label
+        >
+
+        <label
+          >Téléphone <span>*</span><input
+            type="tel"
+            name="phone"
+            placeholder="Entrez votre numéro de téléphone"
+            required
+          /></label
+        >
+
+        <label class="label--message"
+          >Message <span>(Facultatif)</span><textarea
+            name="message"
+            placeholder="Entrez votre message…"
+          /></label
+        >
+      </div>
+      <button type="submit">Envoyer</button>
+    </form>
+  </div>
+</section>
+
+<Toast bind:this={toast}>
+  {#if formSentSuccess}
+    <Icon src={CheckCircle} class="check" solid size="35" />
+    <div class="message">
+      Le formulaire a bien été envoyé.<br />Vous serez bientôt contact.
+    </div>
+  {:else}
+    <Icon src={XCircle} class="error" solid size="35" />
+    <div class="message">
+      Le formulaire n'a pas été correctement envoyé.<br /> Veillez réessayer ou contactez-moi
+      directement.
+    </div>
+  {/if}
+</Toast>
+
+<style lang="scss">
+  @import "./index.scss";
+</style>
